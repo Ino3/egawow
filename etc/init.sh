94d1985817460f4818e80a2e5b788881a55218bd
sak39 <dnsn.isk@gmail.com>
2018-05-28 16:00:33 +0900

    Update init

--- etc/init.sh
+++ etc/init.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 
-sudo sh -c "$(curl -sSfL ) > /etc/wpa_supplicate/wpa_supplicant.conf"
-sudo sh -c "$(curl -sSfL ) > /etc/samba/smb.conf"
+# apt
+function setup_for_apt() {
+	sudo apt update && sudo apt upgrade \
+	sudo apt install emacs \
+	sudo apt install ssh \
+	sudo apt install samba \
+	sudo apt install bc \
+	sudo apt install python \
+	sudo apt install git
+}
 
-hostnamectl set-hostname egawow$(eval )
+function setup_for_wifi() {
+	sudo sh -c "$(curl -sSfL ) > /etc/wpa_supplicate/wpa_supplicant.conf"
+
+}
+
+function setup_for_samba() {
+	sudo sh -c "$(curl -sSfL ) > /etc/samba/smb.conf"
+	sudo systemctl smbd start
+}
+
+function setup_for_hostname() {
+	hostnamectl set-hostname egawow
+}
+
+function setup_for_adding_user() {
+	sudo adduser egawow sudo
+}
+
+function setup_for_egawow_user() {
+	mkdir -p /home/egawow/app
+}
+
