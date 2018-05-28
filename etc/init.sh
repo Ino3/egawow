c9ed878289f5aae7e435d6c74c3e227cb7080a9c
sak39 <dnsn.isk@gmail.com>
2018-05-28 16:50:07 +0900

    Update init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -4,26 +4,28 @@
 function setup_for_apt() {
 	sudo apt update && sudo apt upgrade \
 	sudo apt install emacs \
-	sudo apt install ssh \
 	sudo apt install samba \
 	sudo apt install bc \
 	sudo apt install python \
 	sudo apt install git
 }
 
-function setup_for_wifi() {
-	sudo sh -c "$(curl -sSfL ) > /etc/wpa_supplicate/wpa_supplicant.conf"
-
-}
-
 function setup_for_samba() {
-	sudo sh -c "$(curl -sSfL ) > /etc/samba/smb.conf"
-	sudo systemctl smbd start
+	sudo sh -c "cat <<EOF >> /etc/samba/smb.conf
+	[app]
+        comment = App for emotional detection
+        path = $HOME/app
+        read only = no
+        guest ok = yes
+    EOF
+    "
+	sudo systemctl start smbd
 }
 
-function setup_for_hostname() {
-	hostnamectl set-hostname egawow
+function setup_for_ssh() {
+	sudo systemctl start ssh
 }
+
 
 function setup_for_adding_user() {
 	sudo adduser egawow sudo
@@ -33,3 +35,7 @@ function setup_for_egawow_user() {
 	mkdir -p /home/egawow/app
 }
 
+
+setup_for_apt
+setup_for_samba
+setup_for_ssh
